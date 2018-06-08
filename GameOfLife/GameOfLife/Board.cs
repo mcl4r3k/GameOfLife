b2a2199b91ec5ea08f1736a475938062982f97f7
Luca Malcharek <luca.malcharek@plusserver.com>
2018-06-08 14:58:59 +0200

    init

---
+++ GameOfLife/GameOfLife/Board.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace GameOfLife
+{
+	public class Board
+	{
+		int width;
+		int length;
+
+
+
+		public Board(int x, int y)
+		{
+			width = x;
+			length = y;
+
+		}
+
+
+
+		public int[,] FillGameboard()
+		{
+			Random rnd = new Random();
+
+			int[,] gamefield = new int[width, length];
+			for (int i = 0; i <= width-1; i++)
+			{
+				for (int z = 0; z <= length-1; z++)
+				{
+
+					int rndNumber = rnd.Next(0, 2);
+					gamefield[i, z] = rndNumber;
+				}
+
+			}
+
+			return gamefield;
+		}
+
+		public void PrintGameboard(Array gamefield)
+		{
+			foreach (int element in gamefield)
+			{
+				System.Console.WriteLine(element);
+			}
+
+		}
+
+		public void OutputBoard(int[,] outputarray)
+		{
+
+			string row = "";
+
+			for (int y = 0; y <= width-1; y++)
+			{
+
+				for (int x = 0; x <= length-1; x++)
+				{
+					row += outputarray[x, y].ToString();
+
+				}
+
+				Console.WriteLine(row);
+				row = "";
+
+
+			}
+		}
+
+
+	}
+
+
+}
---
+++ GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace GameOfLife
+{
+	public class main
+	{
+		public static void Main()
+		{
+
+
+			Connways test = new Connways();
+			Board gameboard = new Board(20, 20);
+			int[,] currentboard = gameboard.FillGameboard();
+			int[,] nextgenboard = new int[20, 20];
+
+
+			for (int generation = 0; generation <= 10; generation++)
+			{
+				gameboard.OutputBoard(currentboard);
+				currentboard = test.connwaysRules(currentboard, nextgenboard);
+
+				gameboard.OutputBoard(currentboard);
+				Thread.Sleep(1000);
+				Console.Clear();
+
+
+
+			}
+		}
+
+
+	}
+}
